a31f0b496da4b6fa5ab7e4cc3375b8eeef4173de
Greg Scher <gregscher@gmail.com>
2024-04-29 09:24:51 -0400

    Updated to Well Architected framework

--- .version/version.cmd
+++
@@ -1,2 +0,0 @@
-powershell.exe -NoProfile -ExecutionPolicy Unrestricted -File "%~dp0SetAssemblyVersion.ps1"
-exit /B %ERRORLEVEL%
--- src/Models/Assessment/Excel/CoreReport/Decommissioned_Machines.cs
+++
@@ -1,8 +0,0 @@
-namespace Azure.Migrate.Export.Models
-{
-    public class Decommissioned_Machines
-    {
-        public string MachineName { get; set; }
-        public string MachineId { get; set; }
-    }
-}
